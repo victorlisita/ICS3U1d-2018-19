fc1810a3b86b54724cee6229faafab1292c55d54
robuntu <robuntu@stro.ycdsb.ca>
2019-03-04 11:08:46 -0500

    new drawing functions

--- Masters/1_Drawing_with_Functions/intro_to_functions.py
+++ Masters/1_Drawing_with_Functions/intro_to_functions.py
@@ -4,7 +4,18 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 # define your draw functions
+def grass():
+    arcade.draw_lrtb_rectangle_filled(0, SCREEN_WIDTH, SCREEN_HEIGHT / 3, 0, arcade.color.GREEN)
 
+def bridge():
+    arcade.draw_circle_outline(400, 175, 100, arcade.color.BROWN, 25)
+    arcade.draw_circle_outline(400, 175, 95, arcade.color.BROWN, 25)
+    arcade.draw_circle_outline(400, 175, 90, arcade.color.BROWN, 25)
+
+def river():
+    arcade.draw_rectangle_filled(400, 160, 100, 80, arcade.color.BLUE)
+    arcade.draw_arc_filled(450, 120, 100, 50, arcade.color.BLUE, 180, 360)
+    arcade.draw_rectangle_filled(500, 100, 100, 60, arcade.color.BLUE)
 
 def main():
     arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
@@ -12,6 +23,9 @@ def main():
     arcade.start_render()
 
    # call your draw functions
+    bridge()
+    grass()
+    river()
 
     # Finish and run
     arcade.finish_render()
